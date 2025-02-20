c50c88bd611c069e5016a33561ccb8a72551e3a3
Travis Souther <souther-trav@hotmail.com>
2025-02-20 14:50:23 -0600

    starter code + answer

---
+++ helpers/sql.js
@@ -0,0 +1,27 @@
+const { BadRequestError } = require("../expressError");
+
+/** Generates a SQL string for a partial update query with values
+ * 
+ * This function is used to generate the necessary parts of a SQL query for
+ * updating speific fields in a database table based on the provided data.
+ * It constucts the SET clause of the query dynamically, binding values
+ * to parameters for secure sql execution.
+ * 
+ */
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  const cols = keys.map((colName, idx) =>
+      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
